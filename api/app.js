ffa47b1ca89ebe246fd93b4a291be1bb17c27b0f
rajushrestha1 <facebook101022222@gmail.com>
2025-06-21 14:19:01 +0545

    frontend milauna parne

--- api/app.js
+++ api/app.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
 
 
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/candidates', require('./routes/candidate'));
 
 
 const PORT = process.env.PORT || 3000;
