19f8a1c8f14437e1ca07012102cd53aa121eb094
Saroj Shrestha <saroj@Sarojs-MacBook.local>
2025-06-21 14:47:30 +0545

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import NavBar from './components/NavBar';
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+
+    <NavBar />
+    
   )
 }
+
 
 export default App
